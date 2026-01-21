32204df6d0d2c2fbfb82b3ebcdc831147cef076d
Maruka <emanuelle_carollina@hotmail.com>
2026-01-21 19:59:50 -0300

    feat: develop interactive messaging module for real-time user-assistant interaction

--- main.py
+++ main.py
@@ -13,6 +13,11 @@
       # Django - para criar aplicações web completas, já vem com várias funcionalidades prontas
       # FastAPI - para criar APIs rápidas e performáticas, obriga a estruturar o projeto de 1 maneira
    # visualizar o sistema: streamlit run main.py 
+   # posso usar essa ia no meu site para falar com os usuarios, responder perguntas sobre os produtos, empresa, etc
+   # st.chat_message("user").write() aparece a msg com icone de usuario
+   # st.chat_message("assistant").write() aparece a msg com icone de robo
+   # st.chat_message("Manu").write() aparece com a letra inicial do usuario
+
 
 import streamlit as st
 
@@ -21,4 +26,9 @@ st.write("# Chatbot com IA")  # formato markdown
 texto_user = st.chat_input("Digite sua mensagem")  # armazena a msg na variavel texte_user
 
 if texto_user: 
-    print(texto_user)
+   st.chat_message("user").write(texto_user)
+
+   resposta_ia = "Você me perguntou: " + texto_user # concatenação simples de 2 textos
+   st.chat_message("assistant").write(resposta_ia)
+
+   
